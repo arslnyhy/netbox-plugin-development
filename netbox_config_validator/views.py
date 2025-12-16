b058b8b1fd72095376b4b757e4f509012c9e9310
Arsalan Yahyazadeh <arsalan.jhnson@gmail.com>
2025-12-16 12:27:58 +0200

    step 3 - add template + netbox data
    - create html template
    - modify views.py
    - modify setup.py

--- netbox_config_validator/views.py
+++ netbox_config_validator/views.py
@@ -1,15 +1,28 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.views import View
+from django.db.models import Q
+from dcim.models import Device
 
 
 class DriftCheckView(View):
     """
-    Simple view to check configuration drift between NetBox and devices.
+    View to check configuration drift between NetBox and devices.
     """
     
     def get(self, request):
-        return HttpResponse(
-            "<h1>Config Validator</h1>"
-            "<p>Configuration drift detection is active!</p>"
-        )
+        # Get all devices that have a primary IP configured (IPv4 or IPv6)
+        devices = Device.objects.filter(
+            Q(primary_ip4__isnull=False) | Q(primary_ip6__isnull=False)
+        ).select_related(
+            'primary_ip4',
+            'primary_ip6',
+            'platform',
+            'site',
+        ).order_by('name')
+        
+        context = {
+            'devices': devices,
+        }
+        
+        return render(request, 'netbox_config_validator/drift_check.html', context)
 
--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ setup(
     name='netbox-config-validator',
     version='0.1',
     description='An example NetBox plugin',
-    install_requires=[],
+    install_requires=['netmiko'],
     packages=find_packages(),
     include_package_data=True,
+    package_data={
+        'netbox_config_validator': ['templates/**/*.html'],
+    },
     zip_safe=False,
 )
